ac201c8eabfe34420628be9eafc02f22d6c8e2c1
Akinchan Jain <akinchanjain.7@gmail.com>
2026-08-05 23:00:50 +0530

    Add: subsets - submission-1

---
+++ Data Structures & Algorithms/subsets/submission-1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void f(int i , int n , vector<int> &nums , vector<vector<int>> &ans, vector<int> &temp) {
+         ans.push_back(temp);
+        for(int j =i;  j<n; j++) {
+            temp.push_back(nums[j]);
+            f(j+1, n, nums, ans, temp);
+            temp.pop_back();
+        }
+    }
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        int n = nums.size();
+        vector<int> temp;
+
+        f(0,n,nums,ans,temp);
+        return ans;
+    }
+};
